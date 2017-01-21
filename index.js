fd4b97fb03dbbdeeca82e22910ea37d901a8805e
Jeremy Scheff <jdscheff@gmail.com>
2017-01-21 11:25:41 -0500

    Beginnings of index page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Metalsmith  = require('metalsmith');
+const collections = require('metalsmith-collections');
 const layouts = require('metalsmith-layouts');
 const markdown = require('metalsmith-markdown');
 const permalinks = require('metalsmith-permalinks');
@@ -7,13 +8,20 @@ const processData = require('./plugins/processData');
 Metalsmith(__dirname)
     .source('./src')
     .destination('./build')
-    .use(markdown())
     .use(processData())
+    .use(collections({
+        posts: {
+            pattern: 'posts/*.md',
+            sortBy: 'date',
+            reverse: true,
+        },
+    }))
+    .use(markdown())
     .use(permalinks())
     .use(layouts({
         engine: 'handlebars',
         partials: 'layouts/partials',
     }))
-    .build((err) => {
+    .build((err, files) => {
         if (err) { throw err; }
     });
